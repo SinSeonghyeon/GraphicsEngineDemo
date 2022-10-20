856f0b8e0b4090edf9cca6d3e1f25ca7ea17d4bb
SinSeonghyeon <108246896+SinSeonghyeon@users.noreply.github.com>
2022-10-20 10:58:47 +0900

    Add files via upload

---
+++ Loader/ASEParsingData.h
@@ -0,0 +1,171 @@
+#pragma once
+
+#include "..\Common\Define.h"
+
+using Vector3 = XMFLOAT3;
+using Quaternion = XMVECTOR;
+using Matrix = XMFLOAT4X4;
+
+struct ASEParsingData
+{
+	struct Scene
+	{
+		std::string fileName;
+		unsigned firstFrame;
+		unsigned lastFrame;
+		unsigned frameSpeed;
+		unsigned ticksPerFrame;
+
+		// 사용하지 않는 값입니다.
+
+		Vector3 backgroundStatic;
+		Vector3 ambientStatic;
+	};
+
+	struct MaterialList
+	{
+		struct Material
+		{
+			struct MapDiffuse
+			{
+				std::string bitmap = "";
+			};
+
+			struct MapNormal
+			{
+				std::string bitmap = "";
+			};
+
+			Vector3 ambient;
+			Vector3 diffuse;
+			Vector3 specular;
+			MapDiffuse mapDiffuse;
+			MapNormal mapNormal;
+		};
+
+		unsigned materialCount;
+		std::vector<Material> materialList;
+	};
+
+	struct Mesh
+	{
+		struct Vertex
+		{
+			unsigned index;
+			Vector3 position;
+		};
+
+		struct Face
+		{
+			unsigned index;
+			unsigned vertex[3];
+		};
+
+		struct FaceNormal
+		{
+			struct VertexNormal
+			{
+				unsigned index;
+				Vector3 normal;
+			};
+
+			unsigned index;
+			Vector3 faceNormal;
+			VertexNormal vertexNormal[3];
+		};
+
+		struct TVertex
+		{
+			unsigned index;
+			float u;
+			float v;
+		};
+
+		struct TFace
+		{
+			unsigned faceIndex;
+			unsigned vertexIndex[3];
+		};
+
+		int timeValue;
+		int numVertex;
+		int numFaces;
+		int numTVertex;
+		int numTVFace;
+		std::vector<Vertex> vertexList;
+		std::vector<Face> faceList;
+		std::vector<FaceNormal> faceNormals;
+		std::vector<TVertex> tVertexList;
+		std::vector<TFace> tFaceList;
+	};
+
+	struct WVertex
+	{
+		int index;
+		std::vector<std::pair<int, float>> boneBlendingWeight;
+	};
+
+	struct NodeTM
+	{
+		struct TMRow
+		{
+			unsigned index;
+			float x;
+			float y;
+			float z;
+		};
+
+		std::string nodeName;
+		Matrix tm;
+
+		bool bHasNegativeScale = false;
+	};
+
+	struct TMAnimation
+	{
+		std::string nodeName;
+		std::vector<std::pair<unsigned, Vector3>> controlPosSamples;
+		std::vector<std::pair<unsigned, Quaternion>> controlRotSamples;
+	};
+
+	struct GeoObject
+	{
+		std::string nodeName;
+		std::string nodeParent;
+		NodeTM nodeTM;
+		Mesh mesh;
+		int materialRef;
+		TMAnimation tmAnimation;
+		int numBone;
+
+		std::vector<std::string> boneList;
+
+		std::vector<WVertex> wVertexs;
+	};
+
+	struct HelperObject
+	{
+		std::string nodeName;
+		std::string nodeParent;
+		NodeTM nodeTM;
+		TMAnimation tmAnimation;
+	};
+
+	struct ShapeObject
+	{
+		std::string nodeName;
+		std::string nodeParent;
+		NodeTM nodeTM;
+		TMAnimation tmAnimation;
+	};
+
+	Scene scene;
+
+	MaterialList materialList;
+
+	std::vector<HelperObject> helperObjectList;
+
+	std::vector<ShapeObject> shapeObjectList;
+
+	std::vector<GeoObject> geoObjectList;
+};
---
+++ Loader/Loader_Interface.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "Loader_Interface.h"
+
+#include "Loader_Comet.h"
+
+std::shared_ptr<Loader_Interface> Loader_Interface::_loader = nullptr;
+
+std::shared_ptr<Loader_Interface> Loader_Interface::Get()
+{
+	if (_loader == nullptr)
+	{
+		_loader = std::make_shared<DataLoader::Loader_Comet>();
+		_loader->Initialize();
+	}
+
+	return _loader;
+}
